8952897282d4d75614d14772388f105858327570
AswinSanth <aswinsanth123@gmail.com>
2025-11-25 15:27:04 +0530

    added main.js

--- sever.js
+++ sever.js
@@ -1,3 +1,9 @@
 const sum = (a, b) => {
   console.log(a + b);
 };
+
+//git init
+//git add . =to select all files
+//git add filename =to add select current file
+//git commit -m "message" --->to save
+//git status 
